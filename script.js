28668788cd75558eb6e3756e4fe7a550a5c1a0df
Nabila Ayu Talita <nabilaayutalitaa@gmail.com>
2024-10-22 05:55:45 +0700

    Perubahan 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuicon = document.getElementById("menu-icon");
+const menulist = document.getElementById("menu-list");
+
+menuicon.addEventListener("click", () => {
+    menulist.classList.toggle("hidden");
+
+});
